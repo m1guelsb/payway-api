d756a2547a8534bd2e55d7660037a60f9173d4b6
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-04-08 17:46:58 -0300

    feat: add account dto

---
+++ internal/dto/account-dto.go
@@ -0,0 +1,41 @@
+package dto
+
+import (
+	"time"
+
+	"github.com/m1guelsb/payway-api/internal/domain"
+)
+
+type CreateAccountInput struct {
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+}
+
+type AccountOutput struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Balance   float64   `json:"balance"`
+	ApiKey    string    `json:"api_key"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func ToAccount(input CreateAccountInput) *domain.Account {
+	return &domain.Account{
+		Name:  input.Name,
+		Email: input.Email,
+	}
+}
+
+func FromAccount(account *domain.Account) AccountOutput {
+	return AccountOutput{
+		ID:        account.ID,
+		Name:      account.Name,
+		Email:     account.Email,
+		Balance:   account.Balance,
+		ApiKey:    account.ApiKey,
+		CreatedAt: account.CreatedAt,
+		UpdatedAt: account.UpdatedAt,
+	}
+}
