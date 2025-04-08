b4953785c6b22721510cdda5f6db7ac872cf1de8
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-04-08 17:47:15 -0300

    fix: ajust account repository interface

--- internal/domain/account-repository-interface.go
+++ internal/domain/account-repository-interface.go
@@ -1,7 +1,7 @@
 package domain
 
 type AccountRepository interface {
-	Save(account *Account)
+	Save(account *Account) error
 	FindByApiKey(apiKey string) (*Account, error)
 	FindByID(id string) (*Account, error)
 	UpdateBalance(account *Account) error
