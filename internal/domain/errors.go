d2ce96858718e320f1deed8f0d128294e912bc5c
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-04-22 17:57:00 -0300

    feat: add amount and status errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,12 +3,10 @@ package domain
 import "errors"
 
 var (
-	// ErrAccountNotFound is returned when an account is not found in the database.
-	ErrAccountNotFound = errors.New("account not found")
-	// ErrDuplicatedAPIKey is returned when an account with the same API key already exists.
+	ErrAccountNotFound  = errors.New("account not found")
 	ErrDuplicatedAPIKey = errors.New("api key already exists")
-	// ErrInvoiceNotFound is returned when an invoice is not found in the database.
-	ErrInvoiceNotFound = errors.New("invoice not found")
-	// ErrUnauthorized is returned when an API key is not authorized to access a resource.
-	ErrUnauthorized = errors.New("unauthorized access")
+	ErrInvoiceNotFound  = errors.New("invoice not found")
+	ErrUnauthorized     = errors.New("unauthorized access")
+	ErrInvalidAmount    = errors.New("amount must be greater than zero")
+	ErrInvalidStatus    = errors.New("invalid status, must be one of: pending, approved, rejected")
 )
