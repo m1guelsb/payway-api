830f7fe8e20f0f56ca9b0ba710e6364390943120
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-04-28 20:02:54 -0300

    feat: add invoice repository

--- internal/domain/account-repository-interface.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type AccountRepository interface {
-	Save(account *Account) error
-	FindByApiKey(apiKey string) (*Account, error)
-	FindByID(id string) (*Account, error)
-	UpdateBalance(account *Account) error
-}
