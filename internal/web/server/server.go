ff8eed1f57e8bccfa31c46424648394fa4d18bda
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-04-30 12:05:59 -0300

    feat: add server new routes

--- internal/web/server/server.go
+++ internal/web/server/server.go
@@ -6,28 +6,41 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/m1guelsb/payway-api/internal/service"
 	"github.com/m1guelsb/payway-api/internal/web/handlers"
+	"github.com/m1guelsb/payway-api/internal/web/middleware"
 )
 
 type Server struct {
 	router         *chi.Mux
 	server         *http.Server
 	accountService *service.AccountService
+	invoiceService *service.InvoiceService
 	port           string
 }
 
-func NewServer(accountService *service.AccountService, port string) *Server {
+func NewServer(accountService *service.AccountService, invoiceService *service.InvoiceService, port string) *Server {
 	return &Server{
 		router:         chi.NewRouter(),
 		accountService: accountService,
+		invoiceService: invoiceService,
 		port:           port,
 	}
 }
 
 func (s *Server) ConfigureRoutes() {
 	accountHandler := handlers.NewAccoutHandler(s.accountService)
+	invoiceHandler := handlers.NewInvoiceHandler(s.invoiceService)
+	authMiddleware := middleware.NewAuthMiddleware(s.accountService)
 
 	s.router.Post("/accounts", accountHandler.Create)
 	s.router.Get("/accounts", accountHandler.Get)
+
+	s.router.Group(func(r chi.Router) {
+		r.Use(authMiddleware.Authenticate)
+		s.router.Post("/invoice", invoiceHandler.Create)
+		s.router.Get("/invoice/{id}", invoiceHandler.GetByID)
+		s.router.Get("/invoice", invoiceHandler.ListByAccount)
+	})
+
 }
 
 func (s *Server) Start() error {
